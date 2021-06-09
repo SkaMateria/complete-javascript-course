06065c418ed332241963774fce75a7a038f1cc7e
SkaMateria <skamateria@gmail.com>
2021-06-09 06:31:42 -0400

    Operator Precedence

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -20,3 +20,39 @@
 // bigBig = true
 
 
+
+// arrayOfDogs = ['One', 'Two', 'Three', 'Four']
+
+// let firstName = 'Jonas'
+
+// let lastName = 'Surely'
+
+// console.log(firstName + ' ' + lastName)
+
+// console.log(firstName.concat(' '.concat(lastName)))
+//? .concat() is a method that concatinates same as the '+' operator
+
+// let one = 'FIRST'
+
+// let two = 'Second'
+
+// const templateLiteral = `My ${one} time was her ${two} time`
+
+// console.log(templateLiteral)
+//? ${} is a template literal
+
+// let test = 1;
+// console.log(`First test ${test}`)
+
+// if (test < 2) {
+//   test += 1;
+// }
+
+// console.log(`Second test ${test}`)
+
+// if (test = 2) {
+//   test = 'It is equal!!!'
+// }
+
+// console.log(`Let's see if this works ${test}`)
+
